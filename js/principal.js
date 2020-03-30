d31a5fe90f23487975d7da69a0ce2685f72a5bbe
Marcelomaiuri <marcelobonfimmaiuri@gmail.com>
2020-03-30 09:48:24 -0300

    validação de peso e altura

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,29 @@
+//var titulo = document.querySelector(".titulo");
+// titulo.textContent = "Aparecida Nutricionista";
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-var titulo = document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent = "Aparecida Nutricionista";
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido"); pesoEhValido = falso;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("altura invalida")
+    alguraEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+
+    tdImc.textContent = imc;
+}
